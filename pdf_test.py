4f87eff814a7d6ef816c1c8bc374cffd8329b951
Eilidh Southren <eilidhsouthren@gmail.com>
2018-01-13 19:57:19 +0000

    'test' writing to pdf!

--- pdf_test.py
+++ pdf_test.py
@@ -1,16 +1,7 @@
-import pypdftk as pdf
+import pypdftk as p
+
+datas = { "PlayerName" : "Lea" }
+
+p.fill_form('char_sheet.pdf', datas, 'char_sheet_2.pdf')
 
 
-
-def main():
-    pdf_path = "char_sheet.pdf"
-    datas = { "name": "TEST!"}
-
-    #fields = pdf.get_num_pages(pdf_path)
-    print(pdf.dump_data_fields(pdf_path))
-
-    #generated_pdf = pdf.fill_form(pdf_path, datas, "out.pdf")
-    #out_pdf = pypdftk.merge("/character_sheet_main.pdf", generated_pdf)
-
-if __name__ == '__main__':
-    main()
